a65e5beae650dd97d766ced10ee1e29b9160f15f
Rodgath <rodgath@gmail.com>
2021-05-21 11:08:40 +0300

    added 'GotWpLoginRegister' and its basic methods

--- got-wp-login-register.php
+++ got-wp-login-register.php
@@ -9,3 +9,46 @@ Domain Path: /languages
 Version: 1.0
 Author URI: https://github.com/Rodgath
 */
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) exit;
+
+class GotWpLoginRegister {
+	
+	public function __construct() {
+		
+		/* Internationalization */
+		add_action('init', array($this, 'i18n'));
+		
+	}
+	
+	private function constants() {
+		
+	}
+	
+	/**
+	 * Language translations
+	 */
+	public function i18n() {
+		load_plugin_textdomain('got-wp-lr', false, dirname(plugin_basename(__FILE__)).'/languages/');
+	}
+	
+	public function menu() {
+		
+	}
+	
+	public function includes() {
+		
+	}
+	
+	public function admin_scripts() {
+		
+	}
+	
+	public function frontend_scripts() {
+		
+	}
+	
+}
+
+$GotWpLoginRegister = new GotWpLoginRegister();
