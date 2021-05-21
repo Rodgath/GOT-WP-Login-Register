765a1e09d5958a35139043ce05a675f6ff0ee014
Rodgath <rodgath@gmail.com>
2021-05-21 10:57:22 +0300

    updated got-wp-login-register.php

---
+++ got-wp-login-register.php
@@ -0,0 +1,11 @@
+<?php 
+/*
+Plugin Name: GOT-WP-Login-Register
+Plugin URI: https://github.com/Rodgath/GOT-WP-Login-Register
+Description: Google One Tap Login and Register for WordPress
+Author: Rodgath
+Text Domain: got-wp-lr
+Domain Path: /languages
+Version: 1.0
+Author URI: https://github.com/Rodgath
+*/
