895fc32a67cb15a60ac31df6413c91e290ed428b
rodgath <rodgath@gmail.com>
2021-05-26 05:50:04 +0300

    added 'async' and 'defer' attributes to G client lib script

--- got-wp-login-register.php
+++ got-wp-login-register.php
@@ -15,7 +15,8 @@ if (!defined('ABSPATH')) exit;
 
 class GotWpLoginRegister {
 	
-	public function __construct() {
+	public function __construct()
+	{
 		
 		/* Load constants */
 		$this->constants();
@@ -25,13 +26,17 @@ class GotWpLoginRegister {
 		
 		/* Internationalization */
 		add_action('init', array($this, 'i18n'));
+		
+		/* Add plugin overrides */
+		add_action('plugins_loaded', array($this, 'pluginInit'));
 
 		/* Load frontend styles and scripts */
 		add_action('wp_enqueue_scripts', array($this, 'frontend_scripts'));
 		
 	}
 	
-	private function constants() {
+	private function constants()
+	{
 		defined('GOTWPLR_PREFIX') || define('GOTWPLR_PREFIX', 'gotwplr_');
 		defined('GOTWPLR_DIR') || define('GOTWPLR_DIR', trailingslashit(plugin_dir_path(__FILE__)));
 		defined('GOTWPLR_URI') || define('GOTWPLR_URI', trailingslashit(plugin_dir_url(__FILE__)));
@@ -43,25 +48,52 @@ class GotWpLoginRegister {
 	/**
 	 * Language translations
 	 */
-	public function i18n() {
+	public function i18n()
+	{
 		load_plugin_textdomain('got-wp-lr', false, dirname(plugin_basename(__FILE__)).'/languages/');
 	}
 	
-	public function menu() {
+	public function pluginInit()
+	{
+		add_filter('script_loader_tag', array($this, 'addLibClientAttr'), 10, 3);		
+	}
+	
+	public function menu()
+	{
 		
 	}
 	
-	public function includes() {
+	public function includes()
+	{
 		
 	}
 	
-	public function admin_scripts() {
+	public function admin_scripts()
+	{
 		
 	}
 	
-	public function frontend_scripts() {
-		wp_register_script('gotwplr-client-lib', 'https://accounts.google.com/gsi/client', array(), false, false);
-		wp_enqueue_script('gotwplr-client-lib');
+	public function frontend_scripts()
+	{
+		wp_register_script( 'gotwplr-client-lib', 'https://accounts.google.com/gsi/client', array(), false, false );
+		wp_enqueue_script( 'gotwplr-client-lib' );
+	}
+	
+	/**
+	 * Optimize page loading speed by adding
+	 * Google client library 'async' and 'defer' attributes
+	 */
+	public function addLibClientAttr($tag, $handle, $src)
+	{
+		
+		/* if not our script, do nothing and return original $tag */
+		if ( 'gotwplr-client-lib' !== $handle ) {
+			return $tag;
+		}
+		
+		/* change the script tag by adding 'async defer' and return it. */
+		$tag = '<script src="' . esc_url( $src ) . '" async defer></script>';
+		return $tag;
 	}
 	
 }
