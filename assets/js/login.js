d044df1efc6e8e2baa64526b170725c1efe48ea2
rodgath <rodgath@gmail.com>
2021-05-31 16:37:03 +0300

    updated login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,19 @@
 jQuery(document).ready(function($) {
 	
-	
-	
+	/* Wait until Google client library is loaded */
+	window.onGoogleLibraryLoad = function() {
+		
+		var gotwplrButton = $('#gotwplr-signin-btn'),
+			loginForm = $('#loginform');
+		
+		gotwplrButton.show();
+		
+		if (gotwplrButton.hasClass('form_before')) {
+			gotwplrButton.insertBefore(loginForm);
+		} else if (gotwplrButton.hasClass('form_top')) {
+			gotwplrButton.prependTo(loginForm);
+		} else if (gotwplrButton.hasClass('form_bottom')) {
+			gotwplrButton.appendTo(loginForm);
+		}
+	}
 });
