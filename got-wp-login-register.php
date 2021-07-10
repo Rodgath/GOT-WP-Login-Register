85c59c856df1ae2390f0e339c8cbaf05f63cc655
Rodgath <rodgath@gmail.com>
2021-07-10 06:02:12 +0300

    Updated plugin header block

--- got-wp-login-register.php
+++ got-wp-login-register.php
@@ -1,15 +1,17 @@
 <?php 
-/*
-Plugin Name: GOT WP Login Register
-Plugin URI: https://github.com/Rodgath/GOT-WP-Login-Register
-Description: Google One Tap Login and Register for WordPress.
-Author: Rodgath
-Author URI: https://github.com/Rodgath
-Text Domain: got-wp-lr
-Domain Path: /languages
-Version: 1.0
-License: GPL-2.0+
-License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+/**
+ * Plugin Name:       GOT WP Login Register
+ * Plugin URI:        https://github.com/Rodgath/GOT-WP-Login-Register
+ * Description:       Google One Tap Login and Register for WordPress.
+ * Version:           1.0
+ * Requires at least: 5.0
+ * Requires PHP:      7.0
+ * Author:            Rodgath
+ * Author URI:        https://github.com/Rodgath
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       got-wp-lr
+ * Domain Path:       /languages
 */
 
 // Exit if accessed directly
